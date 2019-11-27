cf89aa0cd3ec714c0f7e703fccea7ecf994a1a86
ms12993 <37820595+ms12993@users.noreply.github.com>
2019-11-27 17:39:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
-bot_id = "REPLACE THIS WITH YOUR BOT ID ONCE BOT IS ADDED TO THE CHAT"
+bot_id = "65297c9584d21d6d75493f8564"
 
 # Called whenever the app's callback URL receives a POST request
 # That'll happen every time a message is sent in the group
