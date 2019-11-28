d1a9416bbc769cd36b38de01069b43858260f1a7
ms12993 <37820595+ms12993@users.noreply.github.com>
2019-11-27 20:07:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def webhook():
 		tagged = []
 	#if 1+1 == 2 and not sender_is_bot(message):
 		#reply(message)
+		
+	if 'ogre' in message['text']:
+		reply('GET OUT ME SWAMP')
 	
 	if message['text'][:5] == '!ogre': 
 
